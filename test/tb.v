ae5baf10a82d9d88f066ffd70afae23ff55dfb43
ha-jer-9 <bouyahiaouihadjer@gmail.com>
2026-09-21 18:42:18 +0100

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -29,11 +29,6 @@ module tb ();
 
   tt_um_workshop_cpu user_project (
 
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
